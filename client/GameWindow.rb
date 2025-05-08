a21ed0740a95623ecba435208e95f8728c8efddd
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-05-08 16:12:30 +0900

    Update GameWindow.rb

--- client/GameWindow.rb
+++ client/GameWindow.rb
@@ -5,7 +5,8 @@ class GameWindow < Gosu::Window
       super(800, 600)
       self.caption = "Danbi Revival"
 
-      connect_server("124.61.178.91", 9000)
+      #connect_server("124.61.178.91", 9000)
+      connect_server("192.168.219.105", 9000)
     end
     
     def update
